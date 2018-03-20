256412f2b28c9cadfb8e95027619d2d5784c8ac3
minjie Shen <mshen@cse.unsw.edu.au>
2018-03-20 11:55:32 +1100

    week4

--- 3/union.c
+++ 3/union.c
@@ -12,10 +12,10 @@ int main () {
   union _all var;
   var.uval = 0x00313233;
 
-  // printf("%x\n", var.uval);
-  // printf("%d\n", var.ival);
-  // printf("%c\n", var.cval);
-  // printf("%s\n", var.sval);
-  // printf("%f\n", var.fval);
-  // printf("%e\n", var.fval);
+  printf("0x%08x\n", var.uval);
+  printf("%d\n", var.ival);
+  printf("%c\n", var.cval);
+  printf("%s\n", var.sval);
+  printf("%f\n", var.fval);
+  printf("%e\n", var.fval);
 }
---
+++ 4/todo.md
@@ -0,0 +1,18 @@
+# Assembly
+- Basic Registers
+- Memory layout
+- Some basic instructions
+    - li la lb lw
+- what's syscall
+- `syscalls` in Assembly (call convention)
+- basic control structures
+    - if
+    - switch
+    - if ||
+    - if &&
+    - while
+    - for loop
+- function call convention
+
+https://courses.cs.washington.edu/courses/cse410/09sp/examples/MIPSCallingConventionsSummary.pdf
+- a bit about stack
